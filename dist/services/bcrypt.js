169edbb5b45f18916c347e050f4c05a1ed32f8f6
Njoli Patrick <ogmaro@gmail.com>
2022-10-25 13:49:00 +0100

    removed dist folder

--- dist/services/bcrypt.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.secret = exports.saltRound = exports.pepper = void 0;
-require("dotenv/config");
-exports.pepper = process.env.BCRYPT_PASSWORD;
-exports.saltRound = parseInt(process.env.SALT_ROUNDS, 10);
-exports.secret = process.env.SECRET;
