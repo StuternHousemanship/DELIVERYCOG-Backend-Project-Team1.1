289ec349b30dc42225e725dc36918b1b9049095e
salim imuzai <salimimuzai@gmail.com>
2022-10-31 21:23:38 +0100

    removed not nullable contrainst from verification code in user database

--- src/V1/APIs/Config/migrations/20221028082513_users.ts
+++ src/V1/APIs/Config/migrations/20221028082513_users.ts
@@ -4,13 +4,13 @@ import { UserType } from '../../Models/User';
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('users', (table) => {
         table.increments('id').primary();
-        table.string('first_name', 50);
-        table.string('last_name', 50);
-        table.string('password_digest', 100);
-        table.bigInteger('phone_number');
-        table.string('email', 100).unique();
+        table.string('first_name', 50).notNullable();
+        table.string('last_name', 50).notNullable();
+        table.string('password_digest', 100).notNullable();
+        table.bigInteger('phone_number').notNullable();
+        table.string('email', 100).unique().notNullable();
         table.integer('verification_code', 10);
-        table.boolean('is_verified').defaultTo('false');
+        table.boolean('is_verified').notNullable().defaultTo('false');
         table.timestamps(true, true);
     });
 }
