b056811728f3a640bc8449108e6044a4fa334a1b
Njoli Patrick <ogmaro@gmail.com>
2022-11-04 21:41:29 +0100

    create new token to destroy old token

--- src/V1/APIs/Repository/auth/auth.repository.ts
+++ src/V1/APIs/Repository/auth/auth.repository.ts
@@ -1,5 +1,6 @@
 import { Encryption } from '../../Utilities/bcrypt';
 import { User, UserType } from '../../Models/User';
+import crypto from 'crypto';
 
 export default class AuthRepository {
     async createUser(user: UserType): Promise<UserType> {
@@ -16,22 +17,7 @@ export default class AuthRepository {
         });
         return newUser;
     }
-    async resetUser(userData: { password: string, email: string }) {
-
-        const user: any = await User.query().where('email', userData.email);
-
-        const hashPassword = await new Encryption().bcrypt(
-            userData.password
-        );
-
-        const updateUserPassword: any = await User.query().where('email', userData.email).patch({
-            password_digest: hashPassword,
-        });
-
-        let newUser: UserType | false = updateUserPassword ? user : false;
-
-        return newUser;
-    }
+    
     async authenticate(email: string, password: string): Promise<UserType[] | undefined> {
         const user: any = await User.query().where('email', email);
         const checkPassword = await new Encryption().compare(
@@ -43,5 +29,22 @@ export default class AuthRepository {
             return undefined;
         }
         return user;
+    }
+    async resetUser(userData: { password: string, email: string }) {
+
+        const user: any = await User.query().where('email', userData.email);
+
+        const hashPassword = await new Encryption().bcrypt(
+            userData.password
+        );
+
+        const updateUserPassword: any = await User.query().where('email', userData.email).patch({
+            password_digest: hashPassword,
+            verification_code: crypto.randomInt(100000, 1000000)
+        });
+
+        let newUser: UserType | false = updateUserPassword ? user : false;
+
+        return newUser;
     }
 }
