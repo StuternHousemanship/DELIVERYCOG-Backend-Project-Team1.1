22cbf64fbfa34e0357a0b7ccf0fa9542e93c4092
jidboris <jidayboris35@gmail.com>
2022-11-07 14:47:33 +0100

    changed to item on orderService

--- src/V1/APIs/Repository/Order/orderRepository.ts
+++ src/V1/APIs/Repository/Order/orderRepository.ts
@@ -3,7 +3,7 @@ import { Orders, OrderType } from '../../Models/orders';
 export default class OrderRepository {
     async createOrder(orders: OrderType): Promise<OrderType> {
         const newOrder: any = await Orders.query().insert({
-            package_name: orders.item,
+            item: orders.item,
             reciever_name: orders.reciever_name,
             reciever_number: orders.reciever_number,
             destination: orders.destination
