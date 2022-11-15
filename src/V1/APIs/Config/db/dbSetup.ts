4b2ed6ff6c1125cfeda4f7a2199d3ac80c4302ee
Njoli Patrick <ogmaro@gmail.com>
2022-11-15 10:24:05 +0100

    updated db config

--- src/V1/APIs/Config/db/dbSetup.ts
+++ src/V1/APIs/Config/db/dbSetup.ts
@@ -2,9 +2,19 @@ import knex from 'knex';
 import knexfile from '../knexfile';
 import { Model } from 'objection';
 
-const setupDb = () => {
+const {NODE_ENV } = process.env;
+
+const Dev = () => {
     const db = knex(knexfile.development);
     Model.knex(db);
 };
+const Prod =()=>{
+    const db = knex(knexfile.production);
+    Model.knex(db);
+}
+
+const setupDb = ()=>{
+    return NODE_ENV === 'production' ? Prod() : Dev();
+}
 
 export default setupDb;
